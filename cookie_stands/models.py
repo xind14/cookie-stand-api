78f26157021419a5d1cf35a8ab4956353afc9d03
Xin Deng <xind1470@gmail.com>
2024-02-22 12:57:41 -0800

    changed model to match lab

--- cookie_stands/models.py
+++ cookie_stands/models.py
@@ -4,16 +4,18 @@ from django.urls import reverse
 
 
 class CookieStand(models.Model):
-    name = models.CharField(max_length=256)
-    rating = models.IntegerField(default=0, blank=True)
-    reviewer = models.ForeignKey(
+    location = models.CharField(max_length=256)
+    owner = models.ForeignKey(
         get_user_model(), on_delete=models.CASCADE, null=True, blank=True
     )
-    description = models.TextField(default="", null=True, blank=True)
+    description = models.TextField(blank=True)
+    hourly_sales = models.JSONField(default=list, null=True)
+    minimum_customers_per_hour = models.IntegerField(default=0)
+    maximum_customers_per_hour = models.IntegerField(default=0)
+    average_cookie_per_sale = models.FloatField(default=0)
 
     def __str__(self):
-        return self.name
+        return self.location
 
     def get_absolute_url(self):
         return reverse('cookiestand_detail', args=[str(self.id)])
-
