4d0201d1e851925d21e097eab72f9c46f31f4a59
Xin Deng <xind1470@gmail.com>
2024-02-22 11:20:21 -0800

    changed things to snacks

--- project/settings.py
+++ project/settings.py
@@ -61,7 +61,7 @@ INSTALLED_APPS = [
     "corsheaders",
     # local
     "accounts",
-    "things",
+    "snacks",
 ]
 
 MIDDLEWARE = [
--- snacks/admin.py
+++ snacks/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Thing
+from .models import Snack
 
 # Register your models here.
-admin.site.register(Thing)
+admin.site.register(Snack)
